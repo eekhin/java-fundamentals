af7b22633f71f8669375602a73c640208b068285
eekhin <eekhin@gmail.com>
2017-08-12 13:45:39 -0700

    Initial Comment

---
+++ src/main/java/com/sqa/eiei/BasicApp.java
@@ -0,0 +1,11 @@
+package com.sqa.eiei;
+
+public class BasicApp {
+
+	public static void main(String[] args) {
+		System.out.print("┏(-_-)┛\n");
+		System.out.print("┏(-_-)┛\n");
+		System.out.print("┏(-_-)┛\n");
+		System.out.print("┏(-_-)┛\n");
+	}
+}
