4090aa1720d9c3bbe435dd43799f166904459739
eekhin <eekhin@gmail.com>
2017-08-23 19:29:19 -0700

    Updated Project Structure

--- src/main/java/com/sqa/eiei/FunApp.java
+++ src/main/java/com/sqa/eiei/FunApp.java
@@ -5,6 +5,8 @@
  */
 package com.sqa.eiei;
 
+import com.sqa.eiei.helpers.*;
+
 /**
  * FunApp //ADDD (description of class)
  * <p>
@@ -17,12 +19,19 @@ package com.sqa.eiei;
  * @since 1.0
  */
 public class FunApp {
-
 	// private static String number;
-	public static void main(String[] args) {
-		// number = app.requestLong();
-		// System.out.println("the number is " + c);
-		long number = AppBasics.requestLong("What is an example of a long number?");
-		System.out.println("Response: " + number);
-	}
+
+	// number = app.requestLong();
+	// System.out.println("the number is " + c);
+	long number = AppBasics.requestLong("What is an example of a long number?");
+
+	// System.out.println("Response: " + number);
+	short number1 = AppBasics.requestShort("What is the Short number");
+
+	// System.out.println("Response: " + number1);
+	float number2 = AppBasics.requestFloat("What is Float number");
+
+	// System.out.println("Response: " + number2);
+	boolean number3 = AppBasics.requestBoolean("What is Boolean number");
+	// System.out.println("Response: " + number3);
 }
--- src/main/java/com/sqa/eiei/helpers/AppBasics.java
+++ src/main/java/com/sqa/eiei/helpers/AppBasics.java
@@ -36,6 +36,23 @@ public class AppBasics {
 		return scanner.nextLine();
 	}
 
+	public static Boolean requestBoolean(String question) {
+		Boolean num = true;
+		String input;
+		System.out.print(question + " ");
+		input = scanner.nextLine();
+		if (input.equalsIgnoreCase("no")) {
+			return false;
+		} else
+			System.out.println("You did not respond to the queston in the correct form");
+		return true;
+	}
+
+	public static char requestChar(String question) {
+		System.out.println(question + "");
+		return scanner.nextLine().charAt(0);
+	}
+
 	// public static int requestInt(String question)
 	// {
 	// int num = 0; // double num =0.0;
@@ -64,7 +81,7 @@ public class AppBasics {
 	}
 
 	public static float requestFloat(String question) {
-		float num; // int num = 0;
+		float num = 0; // int num = 0;
 		String input;
 		System.out.print(question + "");
 		input = scanner.nextLine();
@@ -102,62 +119,8 @@ public class AppBasics {
 		return num;
 	}
 
-	public static Boolean requestBoolean(String question) {
-		Boolean num = true;
-		String input;
-		System.out.print(question + " ");
-		input = scanner.nextLine();
-		try {
-			num = Boolean.parseBoolean(input); // num = Integer.parseInt(input);
-		} catch (NumberFormatException e) {
-			System.out.println("You dsid not supply a valid number [" + input + "].please provide only digits.");
-		}
-		return num;
-	}
-
-	public static char requestChar(String question) {
-		char num;
-		String input;
-		System.out.print(question + " ");
-		input = scanner.nextLine();
-		// try {
-		// num = ; // num = Integer.parseInt(input);
-		// } catch (NumberFormatException e) {
-		System.out.println("You dsid not supply a valid number [" + input + "].please provide only digits.");
-	}return num;
-
-	}
-
 	public static String requestString(String question) {
 		System.out.print(question + "");
 		return scanner.nextLine();
 	}
-	// public static requestLong(String question)
-	// {
-	// long a = 3075;
-	// long b = -1000;
-	//
-	// long c = a+b;
-	// System.out.println(question + " ");
-	// return c;
-	//
-	// }
-	// public static requestShort(String question)
-	// {
-	// short num = 0;
-	// System.out.println(question + "");
-	// return num;
-	// }
-	//
-	// public static double requestBoolean(String question) {
-	// boolean a = true;
-	// System.out.println(question + "");
-	// return num;
-	// }
-	//
-	// public static double requestChar(String question) {
-	// char name;
-	// System.out.println(question + "");
-	// return name;
-	// }
 }
